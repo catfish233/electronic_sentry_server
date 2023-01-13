8a03b6ea40bd1320d38835f662cb80cb37632961
Rong <liu_0818@qq.com>
2023-01-13 14:13:24 +0800

    增加数据库链接

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const routes = require('./routes');
 
 app.use(express.json()); // 添加解析JSON的中间件
 
+// 404处理
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+})
+
 routes(app);
 
 app.get('/', (req, res) => {
@@ -27,7 +32,14 @@ app.delete('/:id', (req, res) => {
   res.status(204).send(req.params);
 });
 
-
 app.listen(port, () => {
   console.log(`Express server listening at http://localhost:${port}`);
+});
+
+// 链接数据库
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/firstDB').then(() => {
+  console.log('数据库链接成功！');
+}).catch(err => {
+  console.log(err, '数据库链接失败！');
 });
